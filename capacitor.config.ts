774505af4dbeb418402eb0bc74a84450b38f0920
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-22 10:46:50 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'counter-app-standalone',
+  webDir: 'www'
+};
+
+export default config;
